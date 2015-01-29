4bce6dcf201b928b814942a0c73d18f2bb11f578
Heff <heff@heff.se>
2015-01-29 14:22:15 +0100

    Fixed output directory. Added some comments for clarification.

--- src/ArtNetOutputAspect.java
+++ src/ArtNetOutputAspect.java
@@ -26,7 +26,9 @@ public class ArtNetOutputAspect {
     @Around("execution(public void *.ArtnetOutput.set_parameters(..))")
     public Object interceptSetParameters(ProceedingJoinPoint invocationPoint) throws Throwable {
 
+        // A map of all ArtNet universes present
         int[][] _unis = (int[][])invocationPoint.getArgs()[0];
+        // A map for each pixel in the grid, containing universe and rgb info.
         int[][][] _map = (int[][][])invocationPoint.getArgs()[1];
 
         num_unis = _unis.length;
@@ -36,28 +38,32 @@ public class ArtNetOutputAspect {
         patch_map = new int[num_unis][1];
         data_length = new int[num_unis];
 
-        for (int i = 0; i < this.num_unis; i++)
+        // Loop through universes and set up structures
+        for (int i = 0; i < num_unis; i++)
         {
             data_length[i] = _unis[i][7];
             patch_map[i] = new int[data_length[i]];
+            // Invalidate all of the patch mapping to begin with
             for (int j = 0; j < data_length[i]; j++) {
                 patch_map[i][j] = -1;
             }
         }
-        for (int x = 0; x < this.size_x; x++) {
-            for (int y = 0; y < this.size_y; y++)
+        // Loop through and fill the patch mapping with data
+        for (int x = 0; x < size_x; x++) {
+            for (int y = 0; y < size_y; y++)
             {
                 int uni_index = _map[x][y][0];
                 int ch_r = _map[x][y][1];
                 int ch_g = _map[x][y][2];
                 int ch_b = _map[x][y][3];
 
-                patch_map[uni_index][ch_r] = ((y * this.size_x + x) * 3 + 0);
-                patch_map[uni_index][ch_g] = ((y * this.size_x + x) * 3 + 1);
-                patch_map[uni_index][ch_b] = ((y * this.size_x + x) * 3 + 2);
+                patch_map[uni_index][ch_r] = ((y * size_x + x) * 3 + 0);
+                patch_map[uni_index][ch_g] = ((y * size_x + x) * 3 + 1);
+                patch_map[uni_index][ch_b] = ((y * size_x + x) * 3 + 2);
             }
         }
 
+        // Initialize buffers with the proper size
         buffer = new byte[size_x * size_y * 3];
         output = new byte[size_x * size_y * 3];
 
@@ -67,18 +73,21 @@ public class ArtNetOutputAspect {
     @Around("execution(public String *.ArtnetOutput.startArtnet())")
     public Object interceptOpenSocket(ProceedingJoinPoint invocationPoint) throws Throwable {
 
+        // Create the output file
         Calendar cal = Calendar.getInstance();
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd__hh-mm-ss");
         String dateStr = dateFormat.format(cal.getTime());
-        path = Paths.get("PiLED_tools\\output", dateStr + ".led");
+        path = Paths.get("GlediatorToDisk\\output", dateStr + ".led");
         Files.createFile(path);
 
+        // Begin the output file with writing matrix dimensions as per the format
         byte[] size = new byte[2];
         size[0] = (byte)size_x;
         size[1] = (byte)size_y;
         Files.write(path, size, StandardOpenOption.APPEND);
 
         socketOpen = true;
+        // Update the GUI with info on the file
         return "Recording to: " + path.getFileName().toString();
     }
 
@@ -86,6 +95,7 @@ public class ArtNetOutputAspect {
     public Object interceptCloseSocket(ProceedingJoinPoint invocationPoint) throws Throwable {
 
         socketOpen = false;
+        // Update the GUI with info on the file
         return "Recorded to: " + path.getFileName().toString();
     }
 
@@ -113,6 +123,7 @@ public class ArtNetOutputAspect {
         if (size_x*size_y <=0 || size_x*size_y > frame.length*3)
             return;
 
+        // Loop through all pixels and assign from frame data
         for (int x = 0; x < size_x; x++) {
             for (int y = 0; y < size_y; y++)
             {
@@ -123,8 +134,10 @@ public class ArtNetOutputAspect {
                 buffer[(3 * index + 2)] = ((byte)frame[index].getBlue());
             }
         }
+
+        // Loop through universes/channels and move to final buffer's order
         int offset = 0;
-        for (int uni = 0; uni < this.num_unis; uni++)
+        for (int uni = 0; uni < num_unis; uni++)
         {
             int count = 0;
             for (int channel = 0; channel < data_length[uni]; channel++)
